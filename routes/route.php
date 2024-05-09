fc475bb09eebd2c868d7f88e184b62bbc60f733a
Praful Rambade <39615916+PrafulRambade@users.noreply.github.com>
2024-05-10 00:32:49 +0530

    This is a new commit for what I originally planned to be amended

---
+++ routes/route.php
@@ -0,0 +1,21 @@
+<?php
+use App\Services\Route;
+use App\Middleware\{
+	Auth,
+	Guest
+};
+
+Route::get('login','LoginController','index',[Guest::class]);
+Route::get('register','RegisterController','index',[Guest::class]);
+Route::post('submit-register','RegisterController','register',[Guest::class]);
+
+Route::post('submit-login','LoginController','login',[Guest::class]);
+
+Route::get('logout','DashboardController','logout',[Auth::class]);
+Route::get('dashboard','DashboardController','index',[Auth::class]);
+
+Route::get('product/{id}/{secondid}','RegisterController','editProduct',[Auth::class]);
+Route::get('product_edit/{id}','RegisterController','editProduct',[Auth::class]);
+Route::get('product_edit/{id}/{p_name}','RegisterController','editProduct',[Auth::class]);
+
+Route::get('','HomeController','index');
